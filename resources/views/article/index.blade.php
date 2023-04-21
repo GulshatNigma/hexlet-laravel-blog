1f5bc10be02990c2b2c03cf6ba2cbd49d77bd19f
Гульшат Нигматуллина <nigma1918@mail.ru>
2023-04-21 14:10:20 +0500

    add articles.create

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    <small><a href="{{ route('articles.create') }}">Создать статью</a></small>
     <h1>Список статей</h1>
     @foreach ($articles as $article)
         <a href="articles/{{ $article->id }}"><h2>{{$article->name}}</h2></a>
